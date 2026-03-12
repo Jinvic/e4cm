0ddc8d3a3b2a9aec25cc023d652c3df52a15d249
Jinvic <2528976435@qq.com>
2026-03-12 16:04:39 +0800

    feat: init db

--- internal/service/db.go
+++ internal/service/db.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"fmt"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+func OpenDB(dbPath string) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("open db: %w", err)
+	}
+	return db, nil
+}
+
+func CloseDB(db *gorm.DB) error {
+	sqlDB, err := db.DB()
+	if err != nil {
+		return fmt.Errorf("get sql db: %w", err)
+	}
+	return sqlDB.Close()
+}
--- internal/service/migrate.go
+++ internal/service/migrate.go
@@ -1,5 +1,21 @@
 package service
 
 func Migrate(csvPath, jsonPath, dbPath string) {
+	// idMap, err := ReadCSV(csvPath)
+	// if err != nil {
+	// 	log.Fatalf("read csv: %v", err)
+	// }
+
+	// twikooComments, err := ReadJSON(jsonPath)
+	// if err != nil {
+	// 	log.Fatalf("read json: %v", err)
+	// }
+
+	// db, err := OpenDB(dbPath)
+	// if err != nil {
+	// 	log.Fatalf("open db: %v", err)
+	// }
+	// defer CloseDB(db)
+
 	// TODO: Implement migration logic
 }
